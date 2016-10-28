7187b5de99940ab7f563b9ab957b2a88773b5c4e
Marta <Marta@MacBook-Pro-Marta.local>
2016-10-29 00:06:24 +0100

    Added contact options

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,9 +15,9 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'dariusz.ban@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$email_subject = "Formularz kontaktowy ze strony:  $name";
 $email_body = "Otrzymałaś nowa wiadomość ze strony dorstyl.pl.\n\n"."Szczegóły wiadomości:\n\nImię i nazwisko: $name\n\nEmail: $email_address\n\nWiadomość:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "Od: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Odpisz do: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
